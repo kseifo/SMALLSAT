c54ab8fe863f129cf23d09c951a721f22eda7e9a
Kareem Seifo <kareem.seifo@student.manchester.ac.uk>
2026-03-06 02:08:14 +0000

    Added DPLL helper data structure

--- Solver.h
+++ Solver.h
@@ -11,6 +11,7 @@ private:
 	std::vector<Clause> clauses;
 	std::vector<Lit> decisionVars;
 	std::vector<Lit> trail;
+	std::vector<int> assigns;
 
 public:
 	Solver() = default;
@@ -19,8 +20,7 @@ public:
 	inline void setVars(int v) { nVars = v; };
 	inline int getVars() const { return nVars; };
 	inline void setClauses(int c) { nClauses = c; };
-	inline const std::vector<Clause>& getClauses() const { return clauses; }
+	inline const std::vector<Clause> &getClauses() const { return clauses; }
 	inline void addClause(Clause c) { clauses.push_back(c); };
 	int solve();
-
 };
