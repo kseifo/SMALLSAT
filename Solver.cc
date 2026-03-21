c4d3f54c3c5cf7ac2b412f05cea96df3875de015
Kareem Seifo <kareem.seifo@student.manchester.ac.uk>
2026-03-21 02:31:59 +0000

    Added simple skeleton for solver implementation

---
+++ Solver.cc
@@ -0,0 +1,21 @@
+#include "Solver.h"
+#include "Types.h"
+#include "Parse.h"
+
+void Solver::setAssigns(int size)
+{
+    assigns.assign(size, -1);
+}
+
+int Solver::solve()
+{
+    return 1;
+}
+
+int main()
+{
+    Solver *s = new Solver();
+    parseFile(*s);
+
+    return 0;
+}
